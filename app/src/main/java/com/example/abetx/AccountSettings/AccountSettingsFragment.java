7195c957f4b7526cf990630f933af7d936f7fb80
Roshaan Tariq <ch.roshaantariq@outlook.com>
2019-03-30 02:39:05 +0500

    Account Settings Fetch Data

--- app/src/main/java/com/example/abetx/AccountSettings/AccountSettingsFragment.java
+++ app/src/main/java/com/example/abetx/AccountSettings/AccountSettingsFragment.java
@@ -7,14 +7,40 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
+import com.example.abetx.Models.User;
 import com.example.abetx.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AccountSettingsFragment extends Fragment {
+    private FirebaseFirestore db;
+
+    private EditText username, description;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.layout_account_settings, container, false);
+        db = FirebaseFirestore.getInstance();
+        username = view.findViewById(R.id.username);
+        description = view.findViewById(R.id.description);
+        DocumentReference docRef = db.collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    User user = task.getResult().toObject(User.class);
+                    username.setText(user.getUser_name());
+                    description.setText(user.getDescription());
+                }
+            }
+        });
         return view;
     }
 }
